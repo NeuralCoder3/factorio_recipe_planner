c2c410c7be35aeafd50f816279ba5a3d29fc1f81
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2024-10-30 23:18:50 +0100

    propagate quality through assembler recipes

--- quality.py
+++ quality.py
@@ -14,10 +14,13 @@ s = Optimize()
 # per default, all resources are zero
 # only for normal (quality=0) resources, we want the amount to be a Real
 resources = defaultdict(lambda: defaultdict(lambda: 0))
+inputs = defaultdict(lambda: defaultdict(lambda: 0))
 for resource in ["iron", "copper"]:
-    resources[resource][0] = Real(resource)
+    v = Real(resource)
+    resources[resource][0] = v
+    inputs[resource][0] = v
 # deep copy
-original_resources = resources.copy()
+objective = sum(sum(quality_amounts.values()) for quality_amounts in inputs.values())
 
 @dataclass
 class Machine:
@@ -33,6 +36,7 @@ class Recipe:
     outputs: dict[str, int]
     name: str | None = None
     accepts_productivity : bool = True
+    accepts_quality : bool = True
     
 assembler = Machine(
     "Assembler", 
@@ -67,12 +71,24 @@ recipes = [
 recipe_amounts = defaultdict(lambda: {})
 
 for ri, recipe in enumerate(recipes):
-    for q in range(max_quality):
+    for q in range(max_quality+1):
         recipe_amounts[ri][q] = Real(f"recipe_{ri}_q{q}")
         for resource, amount in recipe.inputs.items():
             resources[resource][q] -= recipe_amounts[ri][q] * amount
         for resource, amount in recipe.outputs.items():
-            resources[resource][q] += recipe_amounts[ri][q] * amount
+            # resources[resource][q] += recipe_amounts[ri][q] * amount
+            
+            # assuming 4 quality 3 => 10%
+            percent_sum = 0
+            percentage = 0.1
+            for q2 in range(q+1,max_quality+1):
+                percent_sum += percentage
+                resources[resource][q2] += recipe_amounts[ri][q] * amount * percentage
+                percentage /= 10
+            resources[resource][q] += recipe_amounts[ri][q] * amount * (1-percent_sum)
+            # quality modules
+            # if q < max_quality:
+            #     resources[resource][q+1] += recipe_amounts[ri][q] * amount * 0.1
 
 # no resource can be negative
 for resource, quality_amounts in resources.items():
@@ -80,24 +96,47 @@ for resource, quality_amounts in resources.items():
         s.add(amount >= 0)
 
 # we want green circuits
-goal_resource = resources["green_circuits"][0]
+goal_resource = resources["green_circuits"][2]
 s.add(goal_resource >= 1)
-s.minimize(sum(sum(quality_amounts.values()) for quality_amounts in original_resources.values()))
+# objective = sum(sum(quality_amounts.values()) for quality_amounts in original_resources.values())
+s.minimize(objective)
 
 print("Solving...")
+# print("using objective", objective)
 t0 = time.time()
 res = s.check()
 t1 = time.time()
 print(f"Optimization took {t1-t0:.2f} seconds")
+
+def get_float(expr):
+    return float(expr.numerator_as_long())/float(expr.denominator_as_long())
 
 if res == sat:
     print("Solution found")
     print()
     m = s.model()
     print(f"Producing {m.evaluate(goal_resource)} green circuits")
+    
+    print()
+    print("Resources:")
+    for resource, quality_amounts in inputs.items():
+        print(f"  {resource}: ")
+        for quality, amount in sorted(quality_amounts.items(), key=lambda x: x[0]):
+            print(f"    Q{quality}: {get_float(m[amount]):.2f}")
+    
+    
+    print()
+    print("Machines:")
     for ri, recipe in enumerate(recipes):
-        print(f"{recipe.name} crafted in assembler: ")
-        for q in range(max_quality):
-            print(f"  Quality {q}: {m[recipe_amounts[ri][q]]}")
+        print(f"  {recipe.name} crafted in assembler: ")
+        for q in range(max_quality+1):
+            print(f"    Q{q}: {get_float(m[recipe_amounts[ri][q]]):.2f}")
+            
+    print()
+    print("Left over resources:")
+    for resource, quality_amounts in resources.items():
+        print(f"  {resource}: ")
+        for quality, amount in sorted(quality_amounts.items(), key=lambda x: x[0]):
+            print(f"    Q{quality}: {get_float(m.evaluate(amount)):.2f}")
 else:
     print("No solution found")
